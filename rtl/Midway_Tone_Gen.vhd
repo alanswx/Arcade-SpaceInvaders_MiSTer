5927a2b845b92ed12e46326b9f17e0e41ce271f4
Michael Coates <56591865+MacroFPGA@users.noreply.github.com>
2020-11-10 21:17:40 +0000

    Switch to Enable signal

--- rtl/Midway_Tone_Gen.vhd
+++ rtl/Midway_Tone_Gen.vhd
@@ -13,7 +13,7 @@ port(
 	 Tone_out       : out std_logic_vector(15 downto 0);
 	 
 	 -- Clocks and things
-	 CLK_SYS        : in  std_logic; -- 10Mhz (for loading table)
+	 CLK_SYS        : in  std_logic; -- 10Mhz
 	 reset          : in  std_logic  -- high to reset
  );
 end ToneGen;
@@ -22,7 +22,7 @@ architecture struct of ToneGen is
 
  -- Clock dividers
  signal Tone_clk_cnt : std_logic_vector(4 downto 0); -- Main frequency = clock / 20
- signal Tone_Out_clk : std_logic;
+ signal Tone_Out_en  : std_logic := '0';
  
  signal Tone_count   : std_logic_vector(11 downto 0);
  signal Tone_clk     : std_logic;
@@ -37,13 +37,14 @@ begin
 	if rising_edge(CLK_SYS) then
 		if reset='1' then
 			Tone_clk_cnt <= (others=>'0');
-			Tone_Out_clk <= '0';
+			Tone_Out_en  <= '0';
 		else
 			if Tone_clk_cnt = "10011" then
 				Tone_clk_cnt <= (others=>'0');
-				Tone_Out_clk <= not Tone_Out_clk;
+				Tone_Out_en <= '1';
 			else
 				Tone_clk_cnt <= Tone_clk_cnt + 1;
+				Tone_Out_en <= '0';
 			end if;
 		end if;
 	end if;
@@ -53,27 +54,27 @@ end process;
 -- Generator --
 ---------------
 
-process (Tone_Out_clk,reset,Tone_enabled)
+process (CLK_SYS,reset,Tone_enabled)
 begin
 
 	if reset='1' or Tone_enabled='0' then
 		Tone_count <= (others=>'0');
 		Tone_out   <= (others=>'0');
 	else 
-		if rising_edge(Tone_Out_clk) then
-			
-			if Tone_count = x"FFF" then
-				Tone_clk   <= not Tone_clk;
-				Tone_Count <= Tone_High & Tone_Low;
-				if Tone_clk='1' then
-					Tone_out <= x"6000";
+		if rising_edge(CLK_SYS) then
+			if (Tone_Out_en='1') then				
+				if Tone_count = x"FFF" then
+					Tone_clk   <= not Tone_clk;
+					Tone_Count <= Tone_High & Tone_Low;
+					if Tone_clk='1' then
+						Tone_out <= x"6000";
+					else
+						Tone_out <= (others=>'0');
+					end if;
 				else
-					Tone_out <= (others=>'0');
+					Tone_count <= Tone_count + 1;
 				end if;
-			else
-				Tone_count <= Tone_count + 1;
-			end if;
-			
+			end if; -- Tone_Out_EN
 		end if; -- rising clock
 
 	end if; -- reset
